3b9921bbb2f8cd160a05f492ec055ea34b7b70fb
Christoph Hommelsheim <c-hommelsheim@gmx.de>
2016-03-13 11:10:06 +0100

    renabe generator script

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::process::Command;
 
 fn main() {
-    let status = Command::new("python3").arg("generate-sources.py").status().unwrap();
+    let status = Command::new("python3").arg("generate_sources.py").status().unwrap();
     println!("process exited with: {}", status);
 }
